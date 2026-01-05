3b48ff829a63623b66da9e7aa399f8cdf5f0b94c
Ilanewos <ilanewos04@gmail.com>
2026-01-06 07:28:04 +0800

    file untuk upload jembatan dengan mechine learning

---
+++ backend_binclass/middleware/uploadMiddleware.js
@@ -0,0 +1,24 @@
+// middleware/uploadMiddleware.js
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) cb(null, true);
+  else cb(new Error("File harus berupa gambar"), false);
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 2 * 1024 * 1024 } // 2MB
+});
+
+module.exports = upload;
